b1a8d65c55de20658df3857b45fc175a4eadc1ff
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-05-19 21:40:21 -0600

    State example getValue setValue

--- app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
+++ app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
@@ -14,6 +14,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,7 +47,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyStateExample() {
-    var counter = remember {
+    /*var counter = remember {
+        mutableStateOf(0)
+    }*/
+
+    // Mantiene el estado aunque la vista se destruya
+    /*var counter = rememberSaveable {
+        mutableStateOf(0)
+    }*/
+    // Para usar by se importa getValue y setValue
+    var counter by rememberSaveable {
         mutableStateOf(0)
     }
     Column(
@@ -52,10 +64,10 @@ fun MyStateExample() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { counter.value += 1 }) {
+        Button(onClick = { counter += 1 }) {
             Text(text = "Pulsar")
         }
-        Text(text = "He sido pulsado ${counter.value} veces")
+        Text(text = "He sido pulsado $counter veces")
     }
 }
 
