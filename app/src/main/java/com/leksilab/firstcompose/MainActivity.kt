83e291190409bdd958cdafa300705df5a5fa784f
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-05-19 21:31:44 -0600

    State example

--- app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
+++ app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,10 +34,28 @@ class MainActivity : ComponentActivity() {
 //                    MyBox()
 //                    MyColumn()
 //                    MyRow()
-                    MyComplexLayout()
+//                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    var counter = remember {
+        mutableStateOf(0)
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter.value += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${counter.value} veces")
     }
 }
 
@@ -130,6 +151,7 @@ fun DefaultPreview() {
 //        MyBox()
 //        MyColumn()
 //        MyRow()
-        MyComplexLayout()
+//        MyComplexLayout()
+        MyStateExample()
     }
 }
