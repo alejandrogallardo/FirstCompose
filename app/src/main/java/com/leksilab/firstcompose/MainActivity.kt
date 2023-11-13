718d089dd3f3ef44e74473d8398adc6dc83a5cd6
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-11-13 14:24:53 -0600

    text button

--- app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
+++ app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
@@ -66,14 +66,19 @@ fun MyButtonExample() {
         ) {
             Text(text = "HOLA")
         }
-        OutlinedButton(onClick = { /*TODO*/ },
-        Modifier.padding(8.dp),
+        
+        OutlinedButton(onClick = { /*TODO*/ }, 
+            Modifier.padding(8.dp),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color.Magenta,
                 contentColor = Color.Blue,
             disabledBackgroundColor = Color.DarkGray,
             disabledContentColor = Color.Green)) {
             Text(text = "Hola 2")
+        }
+        
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Hola 3")
         }
     }
 }
