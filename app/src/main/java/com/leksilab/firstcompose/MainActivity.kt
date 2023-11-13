2770134f1e353d3ff7adb6a96a01938f739df0a0
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-11-13 14:50:25 -0600

    Imagenes circulares

--- app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
+++ app/src/main/java/com/leksilab/firstcompose/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
@@ -19,7 +18,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -38,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyButtonExample()
+                    MyImage()
                 }
             }
         }
@@ -46,49 +47,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyButtonExample() {
-    // Al momento de dar click el componente se vuelve a recrear
-    // al momento de rotar la pantalla el componente se vuelve a recrear
-    var enable by rememberSaveable {
-        mutableStateOf(true)
-    }
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(24.dp)) {
-        Button(onClick = {
-//            Log.i("Alex-Button", "Esto es un ejemplo")
-              enable = false
-        },
-            enabled = enable,
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta, contentColor = Color.Blue),
-            border = BorderStroke(5.dp, Color.Green)
-        ) {
-            Text(text = "HOLA")
-        }
-        
-        OutlinedButton(onClick = { /*TODO*/ }, 
-            Modifier.padding(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Magenta,
-                contentColor = Color.Blue,
-            disabledBackgroundColor = Color.DarkGray,
-            disabledContentColor = Color.Green)) {
-            Text(text = "Hola 2")
-        }
-        
-        TextButton(onClick = { /*TODO*/ }) {
-            Text(text = "Hola 3")
-        }
-    }
+fun MyImageAdvance() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Ejemplo",
+        // modifier = Modifier.clip(RoundedCornerShape(25f)))
+        // modifier = Modifier.clip(CircleShape) // CircleShape genera un circulo
+        modifier = Modifier.clip(CircleShape)
+            .border(5.dp, Color.Red, CircleShape)
+    )
 }
 
-
+@Composable
+fun MyImage() {
+    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "Ejemplo", alpha = 0.5f)
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FirstComposeTheme {
-        MyButtonExample()
+        MyImage()
     }
 }
